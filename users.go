06baf279631de72b57d05916d6625a2e07bbcfdc
Tricky1975 <rachel.tricky@hotmail.com>
2018-02-27 15:10:02 +0100

    Ask username and password

--- users.go
+++ users.go
@@ -1,14 +1,27 @@
 package main
 
+import "trickyunits/dirry"
+import "trickyunits/qff"
 
 type tuser{
 	name string
+	file string
 	password string
 	ses session
 	totalscore int
 	sessions int
+	ansi bool
 }
 
 var user tuser
+var userdir = dirry("$AppSupport$/$LinuxDot$Phantasar Productions/Who Is The Virus/")
 
+func CreateUser(file,username,password){
+}
 
+func Login(){
+	username:= ai("UserName: ")
+	password:= ai("Password: ")
+	file:=userdir+username
+	if !qff.isfile(file) { CreateUser(file,username,password) }
+}
