48a2933c2f103d2eb90fd7c97150e1873663405a
Tricky1975 <rachel.tricky@hotmail.com>
2018-04-17 14:03:16 +0200

    Random names and virus appointment

--- session.go
+++ session.go
@@ -12,13 +12,20 @@ func CSChat(k string){
 	if CSDBG { fmt.Println("DEBUG> ",k) }
 }
 
+func RandomName() string{
+	rni:=rand.Intn(len(Names))
+	return Names[rni]
+}
+
 func CreateSession(){
 	// Init memory
 	user.ses = session{}
 	s:=&user.ses
 	s.files = map[string] string{}
 	// Define the virus
-	
+	virus:=RandomName()
+	s.files[virus]="*VIRUS*" 
+	CSChat(virus+" is the virus")
 }
 
 
