4c9f2928742b28da5b38b3a71eb8df57c877ae05
Tricky1975 <rachel.tricky@hotmail.com>
2018-04-17 13:40:14 +0200

    Base config + Debug

--- session.go
+++ session.go
@@ -1,5 +1,17 @@
 package main
 
+import(
+	"fmt"
+)
+
+var CSDBG = true // When true debugging information is shown during creation of the session.
+
+func CSChat(k string){
+	if CSDBG { fmt.Println("DEBUG> ",k) }
+}
+
 func CreateSession(){
-	
+	user.ses = session{}
+	s:=&user.ses
+	s.files = map[string] string{}
 }
