1de96da3c26760db3301c894b267039fcac8a312
Tricky1975 <facebook@tbbs.nl>
2018-04-24 19:23:58 +0200

    Cosmetic fix error message

--- sessionrun.go
+++ sessionrun.go
@@ -70,7 +70,7 @@ func init(){
 						} else if qstr.Prefixed(p,"%") {
 							a:=qstr.Right(p,len(p)-1)
 							v,e:=conv.ParseInt(a,10,32)
-							if e!=nil { fmt.Print(red("ERROR! "),yel(e.Error())); return }
+							if e!=nil { fmt.Println(red("ERROR! "),yel(e.Error())); return }
 							allow = allow && int64(len(n))==v
 						} else if p=="boys"  { allow = allow && Sex[n]=="M"
 						} else if p=="girls" { allow = allow && Sex[n]=="F"
